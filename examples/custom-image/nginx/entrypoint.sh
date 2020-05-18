870a6345a5e46622fa14855d781bb81de52a51d5
Lukas Krejci <lkrejci@redhat.com>
2020-05-18 11:04:23 +0200

    Specify a resolver in the nginx configuration so that we can use variables in proxy_pass entries.

--- examples/custom-image/nginx/entrypoint.sh
+++ examples/custom-image/nginx/entrypoint.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+# First we need to finalize the configuration of our server.
+# We need to put in the resolver address and DNS name validity period.
+
+RESOLVER=`cat /etc/resolv.conf | grep nameserver | cut -d' ' -f1 --complement`
+DNS_TTL=${DNS_TTL:-1m}
+
+sed -i -E "s/\\{\\{RESOLVER\\}\\}/$RESOLVER/g" /tmp/nginx/root.conf
+sed -i -E "s/\\{\\{DNS_TTL\\}\\}/$DNS_TTL/g" /tmp/nginx/root.conf
 
 # prepare the location where cm-bump will store the configurations
-mkdir -p /tmp/nginx-locations
+mkdir /tmp/nginx/locations
+
 mkdir -p /tmp/log/cm-bump
-./cm-bump --dir=/tmp/nginx-locations > /tmp/log/cm-bump/cm-bump.log 2>&1 &
+./cm-bump --dir=/tmp/nginx/locations > /tmp/log/cm-bump/cm-bump.log 2>&1 &
 
 nginx -g 'daemon off;'

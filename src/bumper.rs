2b2814bf8dc4dfb27399940c0204b600fee3d418
Lukas Krejci <lkrejci@redhat.com>
2020-05-20 10:42:10 +0200

    Implement a more comprehensive process detection. Now we can detect by PID or process cmdline and we can also check a whole process hierarchy, e.g. PPID=1, cmdline="acme --dominate"

--- src/bumper.rs
+++ src/bumper.rs
@@ -1,11 +1,12 @@
+use log;
+use nix::sys::signal::{self, Signal};
+use nix::unistd::Pid;
+use regex::Regex;
 use std::fs;
 use std::path::Path;
-use thiserror::Error;
-use nix::unistd::Pid;
-use nix::sys::signal::{self, Signal};
+use std::str;
 use std::str::FromStr;
-use regex::Regex;
-use log;
+use thiserror::Error;
 
 #[derive(Debug, Clone, Error)]
 pub enum Error {
@@ -22,48 +23,204 @@ pub enum Error {
 type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, Clone)]
-pub struct Bumper {
-    proc_cmd: Regex,
-    signal: Signal,
-    pid: Option<Pid>,
+pub enum ProcessDetection {
+    Cmdline(Regex),
+    Pid(i32),
 }
 
-impl Bumper {
-    pub fn new(proc_cmd: &str, signal: &str) -> Result<Self> {
-        Ok(Bumper {
-            proc_cmd: Regex::from_str(proc_cmd).map_err(|e| Error::InitError(format!("{}", e)))?,
-            signal: Signal::from_str(signal).map_err(|e| Error::InitError(format!("{}", e)))?,
-            pid: None,
-        })
-    }
+#[derive(Debug, Clone)]
+struct ProcessDetector {
+    detection: ProcessDetection,
+    pid: Option<Pid>,
+    parent: Option<Box<ProcessDetector>>,
+}
 
-    fn ensure_pid(&mut self) -> Result<()> {
-        match self.pid {
-            Some(pid) => {
-                // make sure the pid is still for the process we want
-                let comm = trimmed_contents(format!("/proc/{}/cmdline", pid))?;
-                if !self.proc_cmd.is_match(&comm) {
-                    self.pid = scan_proc(&self.proc_cmd)?;
-                    log::debug!("Process restarted. New PID is {:?}", self.pid);
+#[derive(Debug, Clone)]
+pub struct Bumper {
+    process_tree: ProcessDetector,
+    signal: Signal,
+}
+
+impl ProcessDetector {
+    pub fn pid(&mut self) -> Option<Pid> {
+        log::trace!("Determining pid for {:?}", self);
+        let ppid = match self.parent {
+            Some(ref mut parent) => {
+                log::trace!("PPID required, checking...");
+                match parent.pid() {
+                    Some(ppid) => {
+                        log::trace!("Will check if PPID is {}", ppid);
+                        Some(ppid)
+                    }
+                    // we require a parent process but it could not be found. no point in continuing.
+                    None => {
+                        log::trace!("PPID require yet none found. Bailing.");
+                        return None;
+                    }
                 }
             }
             None => {
-                self.pid = scan_proc(&self.proc_cmd)?;
-                log::debug!("Process PID determined to be {:?}", self.pid);
+                // no parent pid
+                None
             }
+        };
+
+        log::trace!("Checking whether the current PID {:?} is valid", self.pid);
+
+        if !self.valid() {
+            log::trace!(
+                "Current PID {:?} determined not valid. Trying to rediscover.",
+                self.pid
+            );
+            self.pid = match self.find_pid() {
+                Some(new_pid) if ppid.is_some() => match is_parent(&ppid.unwrap(), &new_pid) {
+                    Ok(yes) => {
+                        if yes {
+                            log::trace!("New PID found to be {}", new_pid);
+                            Some(new_pid)
+                        } else {
+                            log::trace!(
+                                "Forgetting the candidate PID {} because PPID doesn't match.",
+                                new_pid
+                            );
+                            None
+                        }
+                    }
+                    Err(e) => {
+                        log::error!(
+                            "Failed to determine parent process of PID {}: {}",
+                            new_pid,
+                            e
+                        );
+                        None
+                    }
+                },
+                Some(new_pid) => {
+                    log::trace!("New PID found to be {}", new_pid);
+                    Some(new_pid)
+                }
+                _ => {
+                    log::trace!("Could not find PID matching the criteria.");
+                    None
+                }
+            };
         }
 
-        Ok(())
+        log::trace!("The PID is {:?}", self.pid);
+
+        self.pid
+    }
+
+    fn find_pid(&self) -> Option<Pid> {
+        match self.detection {
+            ProcessDetection::Cmdline(ref regex) => match scan_proc(&regex) {
+                Ok(res) => res,
+                Err(e) => {
+                    log::error!(
+                        "Failed to scan the process list for process matching {}: {}",
+                        regex,
+                        e
+                    );
+                    None
+                }
+            },
+            ProcessDetection::Pid(ref pid) => {
+                if *pid == 0 {
+                    // special case - PID 0 is mainly useful for specifying PPID of an init-like process
+                    // e.g. the command of a docker container for example. For this case, we always match
+                    // PID 0 successfully.
+                    Some(Pid::from_raw(*pid))
+                } else {
+                    let pid = Pid::from_raw(*pid);
+                    if ProcessDetector::pid_exists(&pid) {
+                        log::trace!("The required PID {} found.", pid);
+                        Some(pid)
+                    } else {
+                        log::trace!("The required PID {} NOT found.", pid);
+                        None
+                    }
+                }
+            }
+        }
+    }
+
+    fn valid(&self) -> bool {
+        log::trace!("Checking whether the current PID {:?} is valid.", self.pid);
+        match self.pid {
+            Some(pid) => match self.detection {
+                ProcessDetection::Cmdline(ref regex) => {
+                    match parse_cmdline(format!("/proc/{}/cmdline", pid)) {
+                        Ok(cmdline) => {
+                            log::trace!(
+                                "Checking whether the cmdline `{}` matches regex `{:?}`",
+                                cmdline,
+                                regex
+                            );
+                            regex.is_match(&cmdline)
+                        }
+                        Err(e) => {
+                            log::warn!("Failed to detect if process {} is still valid: {}", pid, e);
+                            false
+                        }
+                    }
+                }
+                ProcessDetection::Pid(ref expected_pid) => {
+                    if pid.as_raw() == *expected_pid {
+                        log::trace!("Checking whether the required PID {} exists", expected_pid);
+                        ProcessDetector::pid_exists(&pid)
+                    } else {
+                        log::trace!(
+                            "Current PID {} is different from the required PID {}.",
+                            pid,
+                            expected_pid
+                        );
+                        false
+                    }
+                }
+            },
+            None => false,
+        }
+    }
+
+    fn pid_exists(pid: &Pid) -> bool {
+        Path::new(&format!("/proc/{}", pid)).exists()
+    }
+}
+
+impl Bumper {
+    pub fn new(process_tree: Vec<ProcessDetection>, signal: &str) -> Result<Self> {
+        if process_tree.is_empty() {
+            return Err(Error::InitError(
+                "At least 1 process detection needs to be defined.".into(),
+            ));
+        }
+
+        let first = ProcessDetector {
+            detection: process_tree.get(0).unwrap().clone(),
+            pid: None,
+            parent: None,
+        };
+
+        let process_tree = process_tree
+            .iter()
+            .fold(first, |detector, detection| ProcessDetector {
+                detection: detection.clone(),
+                pid: None,
+                parent: Some(Box::from(detector)),
+            });
+
+        Ok(Bumper {
+            process_tree: process_tree,
+            signal: Signal::from_str(signal).map_err(|e| Error::InitError(format!("{}", e)))?,
+        })
     }
 
     pub fn bump(&mut self) -> Result<()> {
-        self.ensure_pid()?;
-
-        match self.pid {
+        match self.process_tree.pid() {
             Some(pid) => {
-                log::debug!("Sending signal {:?} to process {:?}", self.signal, self.pid);
+                log::debug!("Sending signal {:?} to process {:?}", self.signal, pid);
                 signal::kill(pid, self.signal).map_err(|e| Error::SignalError(format!("{}", e)))
-            },
+            }
             _ => {
                 log::info!("No process of the configured name found running. Bump has no effect.");
                 Ok(())
@@ -93,9 +250,13 @@ fn scan_proc(proc_cmd: &Regex) -> Result<Option<Pid>> {
             // now see if the comm of the process is what we're looking for
             let mut comm_path = e.path();
             comm_path.push("cmdline");
-            let comm = trimmed_contents(comm_path)?;
-            
-            log::trace!("Checking `{}` with cmdline `{}`", e.file_name().to_string_lossy(), comm);
+            let comm = parse_cmdline(comm_path)?;
+
+            log::trace!(
+                "Checking `{}` with cmdline `{}`",
+                e.file_name().to_string_lossy(),
+                comm
+            );
 
             if proc_cmd.is_match(&comm) {
                 log::trace!("Matched {}.", comm);
@@ -112,13 +273,7 @@ fn scan_proc(proc_cmd: &Regex) -> Result<Option<Pid>> {
                 Ok(None)
             }
         })
-        .filter(|r| {
-            if let Ok(Some(_)) = r {
-                true
-            } else {
-                false
-            }
-        })
+        .filter(|r| if let Ok(Some(_)) = r { true } else { false })
         .next()
         .unwrap_or(Ok(None))
 }
@@ -127,7 +282,63 @@ fn proc_error(e: &dyn ToString) -> Error {
     Error::ProcError(e.to_string())
 }
 
-fn trimmed_contents<P: AsRef<Path>>(e: P) -> Result<String> {
-    let ret = fs::read_to_string(e).map_err(|e| proc_error(&e))?.trim().to_string();
-    Ok(ret)
+fn parse_cmdline<P: AsRef<Path>>(e: P) -> Result<String> {
+    let bytes = fs::read(e).map_err(|e| proc_error(&e))?;
+    // the cmdline is \0 separated, so we need to convert
+    let cmdline =
+        bytes
+            .split(|b| *b == 0)
+            .map(|a| str::from_utf8(a))
+            .fold(String::new(), |mut acc, s| {
+                if !acc.is_empty() {
+                    acc.push_str(" ");
+                }
+                acc.push_str(s.unwrap_or(""));
+                acc
+            });
+    Ok(cmdline.trim().to_string())
+}
+
+fn is_parent(ppid: &Pid, new_pid: &Pid) -> Result<bool> {
+    let stat =
+        fs::read_to_string(format!("/proc/{}/stat", *new_pid)).map_err(|e| proc_error(&e))?;
+    match stat.rfind(") ") {
+        Some(last_paren) => {
+            let mut splits = stat.split_at(last_paren + 2).1.split(" ");
+            splits.next();
+            let found_ppid = splits.next();
+            match found_ppid {
+                Some(found_ppid) => match found_ppid.parse::<i32>() {
+                    Ok(found_ppid) => Ok(ppid.as_raw() == found_ppid),
+                    Err(e) => {
+                        log::error!(
+                            "Could not parse ppid {} as a number, weird: {}",
+                            found_ppid,
+                            e
+                        );
+                        Ok(false)
+                    }
+                },
+                None => Ok(false),
+            }
+        }
+        None => Ok(false),
+    }
+}
+
+mod test {
+    #[test]
+    fn test_stat_parsing() {
+        // an executable with a ')' in its name... yuck!
+        let stat = "327321 (cm)bump) S 135114 327321 135114 34824 327321 1077936128 3274 0 0 0 21 0 0 0 20 0 9 0 3568036 657534976 6792 18446744073709551615 94252542341120 94252554575081 140734720655776 0 0 0 0 4096 1088 0 0 0 17 6 0 0 0 0 0 94252558218784 94252559146553 94252566843392 140734720662251 140734720662323 140734720662323 140734720667630 0";
+        match stat.rfind(") ") {
+            Some(last_paren) => {
+                let splits: Vec<&str> = stat.split_at(last_paren + 2).1.split(" ").collect();
+                assert_eq!("135114", splits[1])
+            }
+            None => {
+                println!("rfind failed.");
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
+use core::convert::TryFrom;
 use k8s_openapi::api::core::v1::ConfigMap;
 use kube::{
     api::{Api, ListParams},
-    Client,
     config::Config,
+    Client,
 };
 use log;
 use pretty_env_logger::formatted_timed_builder;
+use regex::Regex;
 use std::env;
+use std::str::FromStr;
 use structopt::StructOpt;
-use core::convert::TryFrom;
 
+mod bumper;
 mod operator;
 mod updater;
-mod bumper;
 
 const LOG_ENV_VAR: &str = "CM_LOG";
 
@@ -20,36 +22,50 @@ const LOG_ENV_VAR: &str = "CM_LOG";
 #[structopt(rename_all = "kebab-case")]
 struct Opts {
     /// The directory to which persist the files retrieved from config maps.
-    #[structopt(short,long, env = "CM_DIR")]
+    #[structopt(short, long, env = "CM_DIR")]
     dir: String,
 
     /// The namespace in which to look for the config maps to persist.
-    #[structopt(short,long, env = "CM_NAMESPACE")]
+    #[structopt(short, long, env = "CM_NAMESPACE")]
     namespace: String,
 
     /// Whether to require valid certificate chain. True by default.
-    #[structopt(short,long, env = "CM_TLS_VERIFY")]
+    #[structopt(short, long, env = "CM_TLS_VERIFY")]
     tls_verify: Option<bool>,
 
     /// An expression to match the labels against. Consult the Kubernetes documentation for the
     /// syntax required.
-    #[structopt(short,long, env = "CM_LABELS")]
+    #[structopt(short, long, env = "CM_LABELS")]
     labels: String,
 
-    /// The commandline by which to identify the process to send the signal to. This can be regular expression.
-    #[structopt(short,long, env = "CM_PROC_CMD")]
+    /// The commandline by which to identify the process to send the signal to. This can be a regular expression.
+    /// Ignored if process pid is specified.
+    #[structopt(short = "c", long, env = "CM_PROC_CMD")]
     process_command: Option<String>,
+
+    /// The PID of the process to send the signal to, if known. Otherwise process detection can be used.
+    #[structopt(short = "p", long, env = "CM_PROC_PID")]
+    process_pid: Option<i32>,
+
+    /// The commandline by which to identify the parent process of the process to send signal to. This can be a regular expression.
+    /// Ignored if parent process pid is specified.
+    #[structopt(short = "a", long, env = "CMD_PROC_PARENT_CMD")]
+    process_parent_command: Option<String>,
+
+    /// The PID of the parent process of the process to send the signal to, if known. Otherwise process detection can be used.
+    #[structopt(short = "i", long, env = "CMD_PROC_PARENT_PID")]
+    process_parent_pid: Option<i32>,
 
     /// The name of the signal to send to the process on the configuration files change.
     /// Use `kill -l` to get a list of possible signals and prepend it with "SIG". E.g. "SIGHUP", "SIGKILL", etc.
-    #[structopt(short,long, env = "CM_PROC_SIGNAL")]
+    #[structopt(short, long, env = "CM_PROC_SIGNAL")]
     signal: Option<String>,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     if env::var(LOG_ENV_VAR).is_err() {
-        //std::env::set_var("LOG_ENV_VAR", "info,cm_dump=trace,kube=trace");
+        //std::env::set_var("LOG_ENV_VAR", "info,cm_bump=trace,kube=trace");
         env::set_var(LOG_ENV_VAR, "info,kube=warn");
     }
 
@@ -68,14 +84,15 @@ async fn main() -> anyhow::Result<()> {
     let cms: Api<ConfigMap> = Api::namespaced(client, &opt.namespace);
     let lp = ListParams::default().labels(&opt.labels);
 
-    let bumper = if opt.process_command.is_some() && opt.signal.is_some() {
-        let comm = opt.process_command.unwrap();
-        let signal = opt.signal.unwrap();
-        log::info!("Bumper will look for processes matching `{}` and send `{}` to it on config change.", comm, signal);
-        Some(bumper::Bumper::new(&comm, &signal)?)
-    } else {
-        log::info!("Bumper not configured.");
-        None
+    let bumper = match bumper_config(&opt) {
+        Some((detection, signal)) => {
+            log::info!("Bumper will look for processes matching hierarchy `{:?}` and send `{}` to it on config change.", detection, signal);
+            Some(bumper::Bumper::new(detection, &signal)?)
+        }
+        None => {
+            log::info!("Bumper not configured.");
+            None
+        }
     };
 
     let op = match updater::ConfigUpdater::new(&opt.dir, bumper) {
@@ -89,4 +106,55 @@ async fn main() -> anyhow::Result<()> {
     operator::run(cms, op, lp).await?;
 
     Ok(())
+}
+
+fn bumper_config(opts: &Opts) -> Option<(Vec<bumper::ProcessDetection>, String)> {
+    match opts.signal {
+        Some(ref signal) => {
+            let mut ret = vec![];
+            let parent_process = process_detection_config(
+                &opts.process_parent_command,
+                &opts.process_parent_pid,
+                "the parent",
+            );
+            let process = process_detection_config(&opts.process_command, &opts.process_pid, "the");
+
+            if parent_process.is_some() {
+                ret.push(parent_process.unwrap());
+            }
+
+            if process.is_some() {
+                ret.push(process.unwrap());
+            }
+
+            Some((ret, signal.clone()))
+        }
+        None => None,
+    }
+}
+
+fn process_detection_config(
+    cmd: &Option<String>,
+    pid: &Option<i32>,
+    adjective: &str,
+) -> Option<bumper::ProcessDetection> {
+    match pid {
+        Some(pid) => {
+            if cmd.is_some() {
+                log::warn!("Ignoring {} process command configuration `{}` because {} PID `{}` has been specified.", 
+                    adjective, adjective, cmd.clone().unwrap(), pid);
+            }
+            Some(bumper::ProcessDetection::Pid(*pid))
+        }
+        None => match cmd {
+            Some(cmd) => match Regex::from_str(&cmd) {
+                Ok(regex) => Some(bumper::ProcessDetection::Cmdline(regex)),
+                Err(e) => {
+                    log::error!("Failed to parse {} as a regular expression. Exitting.", e);
+                    std::process::exit(1);
+                }
+            },
+            None => None,
+        },
+    }
 }

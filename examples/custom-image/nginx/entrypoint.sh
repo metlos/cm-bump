713814a58f71021fbc31178f738c1de1d151371d
Lukas Krejci <lkrejci@redhat.com>
2020-05-20 10:42:41 +0200

    Modify the example nginx image to take advantage of the --process-pid

--- examples/custom-image/nginx/entrypoint.sh
+++ examples/custom-image/nginx/entrypoint.sh
@@ -3,7 +3,7 @@
 # First we need to finalize the configuration of our server.
 # We need to put in the resolver address and DNS name validity period.
 
-RESOLVER=`cat /etc/resolv.conf | grep nameserver | cut -d' ' -f1 --complement`
+RESOLVER=`cat /etc/resolv.conf | grep nameserver | head -1 | cut -d' ' -f1 --complement`
 DNS_TTL=${DNS_TTL:-1m}
 
 sed -i -E "s/\\{\\{RESOLVER\\}\\}/$RESOLVER/g" /tmp/nginx/root.conf
@@ -13,6 +13,8 @@ sed -i -E "s/\\{\\{DNS_TTL\\}\\}/$DNS_TTL/g" /tmp/nginx/root.conf
 mkdir /tmp/nginx/locations
 
 mkdir -p /tmp/log/cm-bump
-./cm-bump --dir=/tmp/nginx/locations > /tmp/log/cm-bump/cm-bump.log 2>&1 &
+# We're starting cm bump and setting it up to send the signal configured from the environment
+# to the current process PID. Later we start nginx using exec making it reuse our current PID
+./cm-bump --dir=/tmp/nginx/locations --process-pid=$$ > /tmp/log/cm-bump/cm-bump.log 2>&1 &
 
-nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
